3fd45d882ca74953c1091fc620b7718b16864e38
hamburgerb-bzz <114404289+hamburgerb-bzz@users.noreply.github.com>
2024-09-23 14:58:51 +0200

    done

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-# Dein Code kommt hier hin
+"""     Aufgabe: Kochbuch       """
 
-if __name__ == '__main__':
+import json
+
+
+def load_recipe(recipe_json):
+    return json.loads(recipe_json)
+
+
+def adjust_recipe(original_recipe, num_people):
+    for ingredient in original_recipe["ingredients"]:
+        original_recipe["ingredients"][ingredient] = original_recipe["ingredients"][ingredient] / original_recipe[
+            "servings"] * num_people
+    original_recipe["servings"] = num_people
+    return original_recipe
+
+
+if __name__ == "__main__":
     # Beispiel für die Datenstruktur eines Rezepts
-    recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
+    recipe = ('{"title": "Spaghetti Bolognese", "ingredients": '
+              '{"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}')
     # Dein Code kommt hier hin
+    adjusted_recipe = adjust_recipe(recipe, 2)
+    loaded_recipe = load_recipe(recipe)
